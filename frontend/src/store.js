eefc496fcc0700be281ad77790cf47d220c54716
yuvraj <yuvraj042003@gmail.com>
2023-03-12 19:01:29 +0530

    Revert "his-branch"
    This reverts commit 91516c0e3801dd3d831ace2b2528a978c9900a0d, reversing
    changes made to 312853874a450b63d9d1f1f75956a7461d3b7dde.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,7 @@ import {combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {legacy_createStore as createStore} from 'redux';
-<<<<<<< HEAD
 import { productsReducer, productDetailsReducer, newReviewReducer, newProductReducer, productReducer } from './reducers/productReducer';
-=======
-import { productReducer, productDetailsReducer, newReviewReducer } from './reducers/productReducer';
->>>>>>> e02dc71f51c4fafd5e28ad88b468e0975ad8dcb4
 import { forgotPasswordReducer, profileReducer, userReducer } from './reducers/userReducer';
 import { cartReducer } from './reducers/cartReducer';
 import { myOrdersReducer, newOrderReducer } from './reducers/orderReducer';
@@ -21,13 +17,8 @@ cart:cartReducer,
 newOrder:newOrderReducer,
 myOrders:myOrdersReducer,
 newReview:newReviewReducer,
-<<<<<<< HEAD
 newProduct:newProductReducer,
 product:productReducer,
-=======
-
-
->>>>>>> e02dc71f51c4fafd5e28ad88b468e0975ad8dcb4
 });
 let initialStore = {cart: {
     cartItems: localStorage.getItem("cartItems")
